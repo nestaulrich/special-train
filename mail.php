a86390d8be237542b1e4b6dd212180ba7f6e22d4
Greg U <nestaulrich@gmail.com>
2019-03-15 15:19:06 -0400

    Clean up folder and reset

--- mail.php
+++ mail.php
@@ -1,12 +1,50 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-$formcontent=" From: $name \n Email: $email \n Message: $message";
-$recipient = "gregu@24data.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='mailer.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+        # FIX: Replace this email with recipient email
+        $mail_to = "greg@24data.com";
+        
+        # secret key variable
+        $secretKey = "6LeR7JcUAAAAAM2UR0qbJMSntDZNPHNXSZKppzEE";
+
+        # Sender Data
+        $subject = trim($_POST["subject"]);
+        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $phone = trim($_POST["phone"]);
+        $message = trim($_POST["message"]);
+        
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
+            # Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please complete the form and try again.";
+            exit;
+        }
+        
+        # Mail Content
+        $content = "Name: $name\n";
+        $content .= "Email: $email\n\n";
+        $content .= "Phone: $phone\n";
+        $content .= "Message:\n$message\n";
+
+        # email headers.
+        $headers = "From: $name &lt;$email&gt;";
+
+        # Send the email.
+        $success = mail($mail_to, $subject, $content, $headers);
+        if ($success) {
+            # Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            # Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong, we couldn't send your message.";
+        }
+
+        } else {
+            # Not a POST request, set a 403 (forbidden) response code.
+            http_response_code(403);
+            echo "There was a problem with your submission, please try again.";
+        }
 ?>
---
+++ old-mail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$formcontent=" From: $name \n Email: $email \n Message: $message";
+$recipient = "gregu@24data.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
